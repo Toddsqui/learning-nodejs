96b840c65e23cfa6e4518468d76250ae5493767e
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-12-03 23:14:51 +0900

    edited my first http server.

---
+++ examples/http-hello-world/index.js
@@ -0,0 +1,36 @@
+/* Creating my first Node-based website */
+
+// Show some output
+console.log("Starting the server.");
+
+// "http" is a built-in library (you don't have to install it)
+// Documentation: https://nodejs.org/api/http.html
+var http = require("http");
+
+// callback function is a function provided as argument
+// to another function
+var server = http.createServer(function (request, response) {
+  // the requestion is what we get (from the browser)
+  // the response is what we send to the browser
+
+  // Show some output
+  console.log("> New request: ", request.url, new Date());
+
+  // This will send "Hello World" to the browser (in the response)
+  if (request.url === "/") {
+    response.end("Hello World!");
+  } else {
+    /* TODO  Send not found error */
+  }
+
+});
+
+// Listen on port 9000
+server.listen(9000, function (err) {
+  if (err) {
+    console.error("Cannot start the server");
+    console.error(err);
+    return;
+  }
+  console.log("Server started on port 9000.")
+});
