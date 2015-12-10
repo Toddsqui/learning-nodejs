5c8da7000cd5251ed88ab5464326fd2cb7048d33
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-12-10 23:15:25 +0900

    created foo files to demonstrate how to read and write files with node js.

---
+++ examples/fs-read-write-file/index.js
@@ -0,0 +1,30 @@
+// Require the file system module ("fs")
+// https://nodejs.org/api/fs.html
+var fs = require("fs");
+
+// Create the foo.txt file and put there "The magic number is 42"
+//              / console.log("Writing file")    / console.log("File written");
+// Timeline -*-*------------------------------*-*-->
+//            \ writeFile                    / callback function
+//             *-----------writing file-----*
+// This is an asyncronous call
+fs.writeFile("foo.txt", "The magic number is 42", function (err) {
+  // This function is executed AFTER writing the file on your computer hard disk
+  if (err) {
+    return console.log("Something went wrong writing the file: " + err);
+  }
+  console.log("File written.");
+
+  console.log("Reading the file");
+
+  // you have to move the reading function insdie of the writing function so that thre are no timing issues when you call the writing function
+  fs.readFile("foo.txt", "utf-8", function (err, fileContent) {
+    if(err){
+      return console.log("There is an error", err);
+    }
+    console.log("File Read!");
+    console.log("The content is: " +  fileContent);
+  });
+// end of reading function
+});
+console.log("Writing the file");
