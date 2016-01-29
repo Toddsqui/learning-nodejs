e7ae8e4f726dd9ae6504e300e34a16881470c585
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2016-01-29 15:46:31 +0900

    Adjusted the todd-sum modeul while I was experimenting with making a multiplication module.

---
+++ examples/todd-sum/index.js
@@ -0,0 +1,5 @@
+// npm install todd-sum (you have to do this manually unless you create a package json file)
+var sum = require('todd-sum');
+var mult = require('./multiply.js');
+
+console.log("The Result is: " + mult(10,7));
